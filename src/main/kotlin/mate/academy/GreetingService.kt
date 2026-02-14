5a8c22dbdb4af4260bbbe43b58f515ead5fbaa5c
Bohdan Vorobiei <vbs@ukr.net>
2026-02-14 02:36:57 +0200

    task of the performer

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -2,6 +2,15 @@ package mate.academy
 
 class GreetingService {
     fun getGreetings(clientNames: List<String?>): List<String> {
-        // TODO: implement it
+        val greetings = mutableListOf<String>()
+
+        clientNames.forEach { name ->
+            name?.let {
+                greetings.add("Hello, $it!")
+            }
+        }
+
+        return greetings
     }
 }
+
